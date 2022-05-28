acdd55b657878909b6d993349b114672e4b07a9a
Wallace Martins <wallace159santos@hotmail.com>
2022-05-28 00:54:53 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# 🎬 Netflix Clone
+
+Projeto realizado acompanhando a live do Professor Bonieky Lacerda.
+
+## 📜 Script Disponível
+
+No diretório do projeto, você pode utilizar:
+
+### `npm start`
+
+Executa a aplicação em modo desenvolvimento.\
+Abra [http://localhost:3000](http://localhost:3000) para ver em seu navegador.
+tool wouldn't be useful if you couldn't customize it when you are ready for it.
+
+## 📄 Licença
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
+
+<br />
---
+++ src/App.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+import Tmdb from "./Tmdb"
+
+import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
+import Header from './components/Header';
+
+
+export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedDate] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false)
+
+  useEffect(() => {
+    const loadAll = async () => {
+      // Pegando a lista TOTAL
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+
+      // Pegando o Featured
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedDate(chosenInfo);
+    }
+
+    loadAll()
+  }, [])
+
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10) {
+        setBlackHeader(true)
+      } else {
+        setBlackHeader(false)
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
+  return (
+    <div className='page'>
+
+      <Header black={blackHeader} />
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
+
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
+
+      <footer>
+        Desenvolvido por: <span className="devName" role="img" aria-label="nome e emoji programador">Wallace Martins 👨‍💻</span><br />
+        Direitos de imagem para Netflix<br />
+        Dados pegos do site Themoviedb.org
+      </footer>
+
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Carregando" />
+        </div>
+      }
+    </div>
+  )
+}
